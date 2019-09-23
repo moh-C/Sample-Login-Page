e3c2658810be88158e092c9e6131522188697aaf
Aaron <mohammad.chegini@hotmail.com>
2019-09-23 13:57:09 +0330

    This is done!

--- script.js
+++ script.js
@@ -1,70 +1,65 @@
 $(document).ready(function() {
+  $('#username').focus(() => {
+    $('#userIcon').css({
+      position: 'relative',
+      left: '-10px',
+      color: '#f33a92',
+      'font-size': '22px'
+    });
+    $('#first').addClass('special');
+    $('#username').attr('placeholder', '');
+  });
 
-    $('#username').focus(() => {
-        $('#userIcon').css({
-            'position': 'relative',
-            'left': '-10px',
-            'color': '#f33a92',
-            'font-size': '22px'
-        });
-        $('#first').addClass('special');
-        $('#username').attr('placeholder', '');
-    })
+  $('#username').blur(() => {
+    $('#userIcon').css({
+      position: 'relative',
+      left: '0',
+      color: '#000',
+      'font-size': '18px'
+    });
+    $('#first').removeClass('special');
+    $('#username').attr('placeholder', 'Username');
 
-    $('#username').blur(() => {
-        $('#userIcon').css({
-            'position': 'relative',
-            'left': '0',
-            'color': '#000',
-            'font-size': '18px'
-        })
-        $('#first').removeClass('special');
-        $('#username').attr('placeholder', 'Username');
+    if (!$('#username').val()) {
+      $('#userIcon').css({
+        color: 'red'
+      });
+    } else {
+      $('#userIcon').css({
+        color: 'green'
+      });
+    }
+  });
 
-        if (!$('#username').val()) {
-            $('#userIcon').css({
-                'color': 'red'
-            })
+  $('#password').focus(() => {
+    $('#passIcon').css({
+      position: 'relative',
+      left: '-10px',
+      color: '#f33a92',
+      'font-size': '22px'
+    });
+    $('#second').addClass('special');
+    $('#password').attr('placeholder', '');
+  });
 
-        } else {
-            $('#userIcon').css({
-                'color': 'green'
-            })
-        }
-    })
+  $('#password').blur(() => {
+    $('#passIcon').css({
+      position: 'relative',
+      left: '0',
+      color: '#000',
+      'font-size': '18px'
+    });
+    $('#second').removeClass('special');
+    $('#password').attr('placeholder', 'Password');
 
-
-
-    $('#password').focus(() => {
-        $('#passIcon').css({
-            'position': 'relative',
-            'left': '-10px',
-            'color': '#f33a92',
-            'font-size': '22px'
-        });
-        $('#second').addClass('special');
-        $('#password').attr('placeholder', '');
-    })
-
-    $('#password').blur(() => {
-        $('#passIcon').css({
-            'position': 'relative',
-            'left': '0',
-            'color': '#000',
-            'font-size': '18px'
-        })
-        $('#second').removeClass('special');
-        $('#password').attr('placeholder', 'Password');
-
-        if (!$('#password').val()) {
-            $('#passIcon').css({
-                'color': 'red'
-            })
-        } else {
-            $('#passIcon').css({
-                'color': 'green'
-            })
-        }
-    })
-
-})
+    if (!$('#password').val()) {
+      $('#passIcon').css({
+        color: 'red'
+      });
+    } else {
+      $('#passIcon').css({
+        color: 'green'
+      });
+    }
+  });
+});
